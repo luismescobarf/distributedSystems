05e55831700025cf3d0e4f54c4338dd1b0941eed
luismescobarf <luismescobarf@gmail.com>
2021-01-12 10:45:16 -0500

    Primer programa, estructura básica

---
+++ ejemplosSecuenciales/holaMundo.go
@@ -0,0 +1,11 @@
+package main //Nombre del paquete
+
+//Sección donde se importan otros paquetes o librerías
+import "fmt"
+
+//Definición del main, donde se inicia la ejecición
+func main(){
+	
+	fmt.Println("Hola mundo");//El uso de ; es opcional
+
+}//Se utilizan llaves para el manejo de bloques y contextos
