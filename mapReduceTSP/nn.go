0d5842d70118a145adbb77c8240f1adcfa12339f
luismescobarf <luismescobarf@gmail.com>
2021-03-24 11:23:37 -0500

    Prueba paquete disperso en varios archivos

--- mapReduceTSP/nn.go
+++ mapReduceTSP/nn.go
@@ -11,5 +11,14 @@ func main(){
 	var contenedor tsp.Estructura
 	fmt.Println(contenedor)
 
+	fmt.Println("Cargando la longitud...")
+	contenedor.CargarLongitud(float64(999))
+
+	fmt.Println("Después de cargar la longitud:")
+	fmt.Println(contenedor)
+
+	fmt.Printf("Tipo del contenedor: %T\n Valor del contenedor %v\n",contenedor,contenedor)
+
+
 
 }
